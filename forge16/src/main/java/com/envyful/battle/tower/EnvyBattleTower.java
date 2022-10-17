75c7b16244cb51069232f8c2d0506ba5aabc2d8d
Daniel Norris <dnorris443@gmail.com>
2022-10-17 08:32:52 +0100

    feat(forge16): add and implement config & loading config

--- forge16/src/main/java/com/envyful/battle/tower/EnvyBattleTower.java
+++ forge16/src/main/java/com/envyful/battle/tower/EnvyBattleTower.java
@@ -1,13 +1,17 @@
 package com.envyful.battle.tower;
 
+import com.envyful.api.config.yaml.YamlConfigFactory;
 import com.envyful.api.forge.command.ForgeCommandFactory;
 import com.envyful.api.forge.gui.factory.ForgeGuiFactory;
 import com.envyful.api.forge.player.ForgePlayerManager;
 import com.envyful.api.gui.factory.GuiFactory;
+import com.envyful.battle.tower.config.BattleTowerConfig;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+
+import java.io.IOException;
 
 @Mod("envybattletower")
 public class EnvyBattleTower {
@@ -17,15 +21,30 @@ public class EnvyBattleTower {
     private ForgePlayerManager playerManager = new ForgePlayerManager();
     private ForgeCommandFactory commandFactory = new ForgeCommandFactory();
 
+    private BattleTowerConfig config;
+
     public EnvyBattleTower() {
         instance = this;
         GuiFactory.setPlatformFactory(new ForgeGuiFactory());
 
         MinecraftForge.EVENT_BUS.register(this);
+        this.reloadConfig();
     }
 
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
 
+    }
+
+    public void reloadConfig() {
+        try {
+            this.config = YamlConfigFactory.getInstance(BattleTowerConfig.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public BattleTowerConfig getConfig() {
+        return this.config;
     }
 }
---
+++ forge16/src/main/java/com/envyful/battle/tower/config/BattleTowerConfig.java
@@ -0,0 +1,97 @@
+package com.envyful.battle.tower.config;
+
+import com.envyful.api.config.ConfigLocation;
+import com.envyful.api.config.data.ConfigPath;
+import com.envyful.api.config.type.ConfigRandomWeightedSet;
+import com.envyful.api.config.yaml.AbstractYamlConfig;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+import java.util.List;
+import java.util.Map;
+
+@ConfigSerializable
+@ConfigPath("config/EnvyBattleTower/config.yml")
+public class BattleTowerConfig extends AbstractYamlConfig {
+
+    private Map<String, PossiblePosition> positions = ImmutableMap.of(
+            "example", new PossiblePosition(
+                    new ConfigLocation("world", 1, 1, 1, 1, 1),
+                    new ConfigLocation("world", 1, 1, 1, 1, 1)
+    ));
+
+    private Map<String, TeamPossibilities> teamOptions = ImmutableMap.of(
+            "one", new TeamPossibilities()
+    );
+
+    private int maxFloor = -1;
+
+    public BattleTowerConfig() {
+        super();
+    }
+
+    public List<PossiblePosition> getPositions() {
+        return Lists.newArrayList(this.positions.values());
+    }
+
+    public boolean canContinue(int nextFloor) {
+        if (this.maxFloor == -1) {
+            return true;
+        }
+
+        return nextFloor <= this.maxFloor;
+    }
+
+    public List<TeamPossibilities> getTeamPossibilities() {
+        return Lists.newArrayList(this.teamOptions.values());
+    }
+
+    @ConfigSerializable
+    public static class PossiblePosition {
+
+        private ConfigLocation playerPosition;
+        private ConfigLocation trainerPosition;
+
+        public PossiblePosition(ConfigLocation playerPosition, ConfigLocation trainerPosition) {
+            this.playerPosition = playerPosition;
+            this.trainerPosition = trainerPosition;
+        }
+
+        public PossiblePosition() {
+        }
+
+        public ConfigLocation getPlayerPosition() {
+            return this.playerPosition;
+        }
+
+        public ConfigLocation getTrainerPosition() {
+            return this.trainerPosition;
+        }
+    }
+
+    @ConfigSerializable
+    public static class TeamPossibilities {
+
+        private int startFloor = 1;
+        private int endFloor = 1000;
+        private ConfigRandomWeightedSet<String> teams = new ConfigRandomWeightedSet<>(
+                new ConfigRandomWeightedSet.WeightedObject<>(10, "https://pokepast.es/")
+        );
+
+        public TeamPossibilities() {
+        }
+
+        public int getStartFloor() {
+            return this.startFloor;
+        }
+
+        public int getEndFloor() {
+            return this.endFloor;
+        }
+
+        public ConfigRandomWeightedSet<String> getTeams() {
+            return this.teams;
+        }
+    }
+}
