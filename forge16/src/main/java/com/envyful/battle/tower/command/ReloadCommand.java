4911d8e801772c091b1957d67ffb985c34005ac1
Daniel Norris <dnorris443@gmail.com>
2023-03-14 21:58:42 +0000

    fix(command): allow reload from console

--- forge16/src/main/java/com/envyful/battle/tower/command/ReloadCommand.java
+++ forge16/src/main/java/com/envyful/battle/tower/command/ReloadCommand.java
@@ -7,7 +7,7 @@ import com.envyful.api.command.annotate.executor.CommandProcessor;
 import com.envyful.api.command.annotate.executor.Sender;
 import com.envyful.api.forge.chat.UtilChatColour;
 import com.envyful.battle.tower.EnvyBattleTower;
-import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.command.ICommandSource;
 import net.minecraft.util.Util;
 
 @Command(
@@ -19,7 +19,7 @@ import net.minecraft.util.Util;
 public class ReloadCommand {
 
     @CommandProcessor
-    public void onCommand(@Sender ServerPlayerEntity sender) {
+    public void onCommand(@Sender ICommandSource sender) {
         EnvyBattleTower.getInstance().reloadConfig();
         sender.sendMessage(UtilChatColour.colour("&a&l(!) &aReloaded"), Util.NIL_UUID);
     }
