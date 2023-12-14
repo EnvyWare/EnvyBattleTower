aa1c991bd0fe3ae568eed9abfa0b45dc9c9cbc8a
Daniel Norris <dnorris443@gmail.com>
2023-12-14 00:23:10 +0000

    fix(forge20): update API usage

--- forge20/src/main/java/com/envyful/battle/tower/command/tab/ForgePlayerCompleter.java
+++ forge20/src/main/java/com/envyful/battle/tower/command/tab/ForgePlayerCompleter.java
@@ -9,17 +9,7 @@ import net.minecraftforge.server.ServerLifecycleHooks;
 import java.lang.annotation.Annotation;
 import java.util.List;
 
-public class ForgePlayerCompleter implements TabCompleter<String, ForgeEnvyPlayer> {
-
-    @Override
-    public Class<ForgeEnvyPlayer> getSenderClass() {
-        return ForgeEnvyPlayer.class;
-    }
-
-    @Override
-    public Class<String> getCompletedClass() {
-        return String.class;
-    }
+public class ForgePlayerCompleter implements TabCompleter<ForgeEnvyPlayer> {
 
     @Override
     public List<String> getCompletions(ForgeEnvyPlayer sender, String[] currentData, Annotation... completionData) {
