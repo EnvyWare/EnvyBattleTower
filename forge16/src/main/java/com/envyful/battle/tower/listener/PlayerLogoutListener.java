b2002ff18660f48f097e17d558e4868c85dfc5f4
Daniel Norris <dnorris443@gmail.com>
2025-02-16 16:36:05 +0000

    ref(listener): update method usages

--- forge16/src/main/java/com/envyful/battle/tower/listener/PlayerLogoutListener.java
+++ forge16/src/main/java/com/envyful/battle/tower/listener/PlayerLogoutListener.java
@@ -1,7 +1,7 @@
 package com.envyful.battle.tower.listener;
 
 import com.envyful.battle.tower.EnvyBattleTower;
-import com.envyful.battle.tower.player.BattleTowerAttribute;
+import com.envyful.battle.tower.api.attribute.BattleTowerAttribute;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -10,7 +10,7 @@ public class PlayerLogoutListener {
 
     @SubscribeEvent
     public static void onPlayerLogout(PlayerEvent.PlayerLoggedOutEvent event) {
-        var player = EnvyBattleTower.getInstance().getPlayerManager().getPlayer((ServerPlayerEntity) event.getPlayer());
+        var player = EnvyBattleTower.getPlayerManager().getPlayer((ServerPlayerEntity) event.getPlayer());
 
         if (player == null) {
             return;
