f504495080d50621da9ed42f160c8a02eb3ec566
Daniel Norris <dnorris443@gmail.com>
2025-02-16 19:05:12 +0000

    ref(api): add comments for TeamPossibilities

--- forge16/src/main/java/com/envyful/battle/tower/api/TeamPossibilities.java
+++ forge16/src/main/java/com/envyful/battle/tower/api/TeamPossibilities.java
@@ -2,15 +2,25 @@ package com.envyful.battle.tower.api;
 
 import com.envyful.api.config.type.ConfigRandomWeightedSet;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
 
 @ConfigSerializable
 public class TeamPossibilities {
 
+    @Comment("The lowest floor this team can be encountered on (if you want it to be available on all floors, set this to 0)")
     private int startFloor = 0;
+    @Comment("The highest floor this team can be encountered on (if you want it to be available on all floors, set this to the max floor)")
     private int endFloor = 1000;
-    private ConfigRandomWeightedSet<PokePaste> teams = ConfigRandomWeightedSet.builder(new PokePaste("https://pokepast.es/7fb798994199787a"), 10).build();
+    @Comment("A weighted set of teams that can be encountered on this floor. The higher the weight, the more likely it is to be selected")
+    private ConfigRandomWeightedSet<PokePaste> teams = ConfigRandomWeightedSet.builder(new PokePaste("https://pokepast.es/8c84d94a0da05c13"), 10).build();
 
     public TeamPossibilities() {
+    }
+
+    public TeamPossibilities(int startFloor, int endFloor, ConfigRandomWeightedSet<PokePaste> teams) {
+        this.startFloor = startFloor;
+        this.endFloor = endFloor;
+        this.teams = teams;
     }
 
     public int getStartFloor() {
