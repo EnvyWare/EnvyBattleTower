cb797312c6a8c7e840233d7d2b547be0828c8f26
Daniel Norris <dnorris443@gmail.com>
2025-02-16 16:35:50 +0000

    ref(project): update EnvyBattleTower class in prep for multi battle tower system

--- forge16/src/main/java/com/envyful/battle/tower/EnvyBattleTower.java
+++ forge16/src/main/java/com/envyful/battle/tower/EnvyBattleTower.java
@@ -3,24 +3,21 @@ package com.envyful.battle.tower;
 import com.envyful.api.concurrency.UtilLogger;
 import com.envyful.api.config.yaml.YamlConfigFactory;
 import com.envyful.api.database.Database;
-import com.envyful.api.database.leaderboard.Order;
+import com.envyful.api.forge.chat.ITextComponentTextFormatter;
 import com.envyful.api.forge.command.ForgeCommandFactory;
 import com.envyful.api.forge.command.parser.ForgeAnnotationCommandParser;
 import com.envyful.api.forge.gui.factory.ForgeGuiFactory;
+import com.envyful.api.forge.platform.ForgePlatformHandler;
 import com.envyful.api.forge.player.ForgeEnvyPlayer;
 import com.envyful.api.forge.player.ForgePlayerManager;
 import com.envyful.api.gui.factory.GuiFactory;
-import com.envyful.api.leaderboard.Leaderboard;
 import com.envyful.api.platform.PlatformProxy;
+import com.envyful.battle.tower.api.attribute.BattleTowerAttribute;
 import com.envyful.battle.tower.command.BattleTowerCommand;
-import com.envyful.battle.tower.command.tab.ForgePlayerCompleter;
 import com.envyful.battle.tower.config.BattleTowerConfig;
 import com.envyful.battle.tower.config.BattleTowerGraphics;
 import com.envyful.battle.tower.config.BattleTowerLocale;
-import com.envyful.battle.tower.config.BattleTowerQueries;
 import com.envyful.battle.tower.listener.PlayerLogoutListener;
-import com.envyful.battle.tower.player.BattleTowerAttribute;
-import com.envyful.battle.tower.player.BattleTowerEntry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -31,7 +28,6 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @Mod("envybattletower")
 public class EnvyBattleTower {
@@ -47,12 +43,14 @@ public class EnvyBattleTower {
     private BattleTowerLocale locale;
     private BattleTowerGraphics graphics;
     private Database database;
-    private Leaderboard<BattleTowerEntry> leaderboard;
 
     public EnvyBattleTower() {
         instance = this;
         UtilLogger.setLogger(LOGGER);
         GuiFactory.setPlatformFactory(new ForgeGuiFactory());
+        PlatformProxy.setPlayerManager(this.playerManager);
+        PlatformProxy.setHandler(ForgePlatformHandler.getInstance());
+        PlatformProxy.setTextFormatter(ITextComponentTextFormatter.getInstance());
 
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(PlayerLogoutListener.class);
@@ -63,28 +61,13 @@ public class EnvyBattleTower {
         this.reloadConfig();
 
         this.database = this.config.getDatabaseDetails().createDatabase();
-
-
-
-        this.leaderboard = Leaderboard.builder(BattleTowerEntry.class)
-                .database(this.database)
-                .cacheDuration(TimeUnit.MINUTES.toMillis(10))
-                .formatter(BattleTowerEntry::fromQuery)
-                .order(Order.DESCENDING)
-                .table("envy_battle_tower_players")
-                .column("floor_reached")
-                .pageSize(10)
-                .build();
-
-        getDatabase()
-                .update(BattleTowerQueries.CREATE_TABLE)
-                .executeAsync();
+        this.playerManager.getAdapter(BattleTowerAttribute.class).initialize();
     }
 
     @SubscribeEvent
     public void onCommandRegister(RegisterCommandsEvent event) {
+        this.playerManager.setGlobalSaveMode("SQL");
         this.playerManager.registerAttribute(BattleTowerAttribute.class, BattleTowerAttribute::new);
-        this.commandFactory.registerCompleter(new ForgePlayerCompleter());
 
         this.commandFactory.registerInjector(ForgeEnvyPlayer.class, (sender, args) -> {
             ForgeEnvyPlayer onlinePlayer = this.playerManager.getOnlinePlayer(args[0]);
@@ -123,10 +106,6 @@ public class EnvyBattleTower {
 
     public static Database getDatabase() {
         return instance.database;
-    }
-
-    public Leaderboard<BattleTowerEntry> getLeaderboard() {
-        return this.leaderboard;
     }
 
     public static ForgePlayerManager getPlayerManager() {
