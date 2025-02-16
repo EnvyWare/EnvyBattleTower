658fcc6092da23291eae892933987d585d32d3ef
Daniel Norris <dnorris443@gmail.com>
2025-02-16 16:39:08 +0000

    feat(locale): add explanatory comments, time format and date format

--- forge16/src/main/java/com/envyful/battle/tower/config/BattleTowerLocale.java
+++ forge16/src/main/java/com/envyful/battle/tower/config/BattleTowerLocale.java
@@ -1,9 +1,12 @@
 package com.envyful.battle.tower.config;
 
 import com.envyful.api.config.data.ConfigPath;
+import com.envyful.api.config.type.DateFormatConfig;
+import com.envyful.api.config.type.TimeFormatConfig;
 import com.envyful.api.config.yaml.AbstractYamlConfig;
 import com.google.common.collect.Lists;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
 
 import java.util.List;
 
@@ -11,9 +14,16 @@ import java.util.List;
 @ConfigPath("config/EnvyBattleTower/locale.yml")
 public class BattleTowerLocale extends AbstractYamlConfig {
 
+    @Comment("This is the message that will be sent to the player if they have a blacklisted pokemon in their party")
     private List<String> blacklistedPokemonError = Lists.newArrayList(
             "&c&l(!) &cError: you have a blacklisted pokemon in your party so you cannot enter the battle tower! %pokemon%"
     );
+
+    @Comment("This is used for formatting any times that appear in GUIs, or in messages")
+    private TimeFormatConfig timeFormat = new TimeFormatConfig();
+
+    @Comment("This is used for formatting any dates that appear in GUIs, or in messages")
+    private DateFormatConfig dateFormat = new DateFormatConfig("dd/MM/yyyy");
 
     public BattleTowerLocale() {
         super();
@@ -21,5 +31,13 @@ public class BattleTowerLocale extends AbstractYamlConfig {
 
     public List<String> getBlacklistedPokemonError() {
         return this.blacklistedPokemonError;
+    }
+
+    public TimeFormatConfig getTimeFormat() {
+        return this.timeFormat;
+    }
+
+    public DateFormatConfig getDateFormat() {
+        return this.dateFormat;
     }
 }
