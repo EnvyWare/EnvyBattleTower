643b5b5031efdb9ba41598fb67d41760b78dbfcc
Daniel Norris <dnorris443@gmail.com>
2025-02-16 19:05:55 +0000

    ref(api): add comments for PokePaste configs

--- forge16/src/main/java/com/envyful/battle/tower/api/PokePaste.java
+++ forge16/src/main/java/com/envyful/battle/tower/api/PokePaste.java
@@ -6,19 +6,23 @@ import com.envyful.battle.tower.EnvyBattleTower;
 import com.google.common.collect.Lists;
 import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
 
 import java.util.List;
 
 @ConfigSerializable
 public class PokePaste {
 
+    @Comment("PokePaste URL to load the team from")
     private String paste;
     private transient List<Pokemon> team;
+    @Comment("Commands to run when the player wins a battle against this team")
     private ConfigRandomWeightedSet<List<String>> playerWinCommands = new ConfigRandomWeightedSet<>(
-            new ConfigRandomWeightedSet.WeightedObject<>(10, Lists.newArrayList("broadcast %player%"))
+            new ConfigRandomWeightedSet.WeightedObject<>(10, Lists.newArrayList("minecraft:tell %player% Well done you won!"))
     );
+    @Comment("Commands to run when the player loses a battle against this team")
     private ConfigRandomWeightedSet<List<String>> playerLossCommands = new ConfigRandomWeightedSet<>(
-            new ConfigRandomWeightedSet.WeightedObject<>(10, Lists.newArrayList("broadcast %player%"))
+            new ConfigRandomWeightedSet.WeightedObject<>(10, Lists.newArrayList("minecraft:tell %player% You lost! Better luck next time"))
     );
 
     public PokePaste(String paste) {
